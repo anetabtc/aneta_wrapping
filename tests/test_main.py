4ead67df8ad700bd7fae979979bdd6491aa97428
Willie McClinton <samsa@Willies-MacBook-Pro-2.local>
2022-04-03 01:29:32 -0400

    passed all tests locally

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,11 @@
 """Tests for main.py."""
 
-import os
-import shutil
 import sys
-from typing import Callable
 
-import pytest
-from aneta-wrapping.src.main import main
+from aneta_wrapping.src.main import main
 
 
 def test_main():
     """Tests for main.py."""
     sys.argv = []
-    with pytest.raises(NotImplementedError):
-        main()
+    main()
